c726ffb40aa4f538bddefcd9c6614a47e3fc489d
Jirayu <jirayu.srisawat.education@gmail.com>
2025-09-21 10:34:11 +0700

    Align lyrics scaffold background with player

--- app/src/main/java/com/zionhuang/music/ui/component/Lyrics.kt
+++ app/src/main/java/com/zionhuang/music/ui/component/Lyrics.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -306,6 +307,8 @@ fun Lyrics(
 
     Scaffold(
         modifier = modifier.fillMaxSize(),
+        containerColor = Color.Transparent,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
             PlayerTopBar(
                 viewMode = viewMode,
